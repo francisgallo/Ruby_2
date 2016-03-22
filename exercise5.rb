f8dbac1e53d837af500c489e6387c92d83c91881
Francis Gallo <francisgallo@Franciss-MacBook-Pro.local>
2016-03-22 15:23:45 -0400

    finished question

--- exercise5.rb
+++ exercise5.rb
@@ -1,6 +1,7 @@
 def temperature
   puts "what is the temperature in Fahrenheit currently?"
   f=gets.chomp.to_i
-  puts f
+c= ((f-32) *5/9)
+puts "your current temperature is #{c} Celsius"
 end
 temperature
