9a6b3f643bcc1b90c6e9dec785b09557ab7c63a1
Francis Gallo <francisgallo@Franciss-MacBook-Pro.local>
2016-03-23 15:01:43 -0400

    on sort

--- exercise_6.rb
+++ exercise_6.rb
@@ -1,2 +1,29 @@
-grocery_list = ["Strawberries", "milk" , "Chicken", "apple","rice"]
-puts grocery_list
+grocery_list = ["Strawberries", "milk" , "Chicken", "apple", "rice"]
+
+grocery_list.each {|e| puts "* #{e}"}
+
+def add(the_list)
+  puts "any items you wish to add to your list?"
+  item=gets.chomp
+    g2=the_list.push(item)
+    puts g2
+  end
+
+def list(l)
+  g1=l.size
+  puts "You Currently  have  #{g1} items in your list"
+end
+
+add(grocery_list)
+list(grocery_list)
+
+ if grocery_list.include?("banana")
+   puts "You need to pick up banana's today"
+ else
+   puts "You do not need to pick up banana's today"
+ end
+
+ puts grocery_list[1]
+
+sort=grocery_list.sort
+sort.each {|e| puts "* #{e}"}
