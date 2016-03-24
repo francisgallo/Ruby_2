ce8681a665e56f531beb23edf9d404a26f5a2dd4
Francis Gallo <francisgallo@Franciss-MacBook-Pro.local>
2016-03-23 20:29:25 -0400

    cleaned up final display

--- exercise7.rb
+++ exercise7.rb
@@ -1,9 +1,13 @@
+dashes = "-" * 50
+
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22
 }
 
+
+puts "\nQuestions 2:\n"
 def show(var)
   var.each do |name,size|
   puts " #{name}:  #{size} students"
@@ -11,24 +15,46 @@ def show(var)
 end
 
 show(students)
+puts dashes
 
+puts "\n Question3:\n"
 students[:cohort4] = 43
+show (students)
+puts dashes
 
+puts "\nQuestion 4: \n"
 
 puts students.keys
+
+  puts dashes
+
+puts "\nQuestion 5: \n\n"
 
 students.each do |name,size|
   students[name] = (size*1.05).round
 end
 
 show(students)
+puts dashes
 
-
+puts "\nQuestion 6: \n\n"
 students.delete(:cohort2)
 puts students
 
-num_students = students.values
+puts dashes
+# num_students = students.values
+#
+#
+# s = num_students.inject(0) {|sum , values| sum + values}
+# puts "your total number of students are #{s}"
 
+puts "\nQuestion 7: \n\n"
 
-s = num_students.inject(0) {|sum , values| sum + values}
-puts "your total number of students are #{s}"
+students_total = 0
+students.each do |k, v|
+  students_total = students_total + v
+end
+
+puts "Total students accross all cohorts: #{students_total}"
+
+puts dashes
--- exercise_6.rb
+++ exercise_6.rb
@@ -1,10 +1,15 @@
-grocery_list = ["strawberries", "milk" , "chicken", "salmon", "rice"]
-
+dashes= ("-")*50
+grocery_list = ["strawberries", "milk" , "chicken", "salmon"]
 grocery_list.each {|item| puts "* #{item}"}
+
+puts "Question 1\n"
+grocery_list << "rice"
+grocery_list.each {|item| puts "* #{item}"}
+puts dashes
 
 def add(the_list)
   puts "any items you wish to add to your list? if your list is correct type nothing"
-  item=gets.chomp
+  item=gets.chomp.downcase
       if item == "nothing"
       puts "you did not add anything"
         else
@@ -13,28 +18,44 @@ def add(the_list)
       end
   end
 
+
+
 def list(l)
+  puts "\n question 2!"
   g1=l.size
   puts "You Currently  have  #{g1} items in your list"
 end
 
+
 add(grocery_list)
 list(grocery_list)
+puts dashes
+
+puts "\n Question 3! \n \n"
 
  if grocery_list.include?("banana")
    puts "You need to pick up banana's today"
  else
    puts "You do not need to pick up banana's today"
  end
+puts dashes
 
+puts "\n Question 4! \n\n"
  puts grocery_list[1]
+puts dashes
 
+puts "\n Question 5 \n\n"
 sorted_list= grocery_list.sort
 
 sorted_list.each {|item| puts "* #{item}"}
+puts dashes
+
+puts" \n Question 6! \n\n"
 
 grocery_list.delete_at(3)
 
 sorted_list=grocery_list.sort
 
 puts sorted_list
+
+puts dashes
